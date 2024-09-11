cb2e8b565024e1a6ace19cbb4a05410a2447c53a
Jack Greiner <jack@emoss.org>
2024-09-10 20:50:19 -0400

    Whitespace

--- src/main/java/tools/sctrade/companion/utils/ScreenshotUtil.java
+++ src/main/java/tools/sctrade/companion/utils/ScreenshotUtil.java
@@ -30,98 +30,98 @@ import org.slf4j.LoggerFactory;
 
 public class ScreenshotUtil {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ScreenshotUtil.class);
+  private static final Logger LOG = LoggerFactory.getLogger(ScreenshotUtil.class);
 
-    public static BufferedImage createScreenshot() {
-        if (isWayland()) {
-            LOG.debug("Wayland detected");
-            return createDbusScreenshot();
-        }
-        return createRobotScreenshot();
-    }
+  public static BufferedImage createScreenshot() {
+      if (isWayland()) {
+          LOG.debug("Wayland detected");
+          return createDbusScreenshot();
+      }
+      return createRobotScreenshot();
+  }
 
-    private static boolean isWayland() {
-        return "wayland".equalsIgnoreCase(System.getenv("XDG_SESSION_TYPE"));
-    }
+  private static boolean isWayland() {
+      return "wayland".equalsIgnoreCase(System.getenv("XDG_SESSION_TYPE"));
+  }
 
-    private static BufferedImage createRobotScreenshot() {
-        try {
-            LOG.debug("Taking robot screenshot");
-            Robot robot = new Robot();
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            Rectangle screenRect = new Rectangle(screenSize);
-            BufferedImage shot = robot.createScreenCapture(screenRect);
-            return shot;
-        } catch (Exception ex) {
-            LOG.error("Error creating robot screenshot", ex);
-            return null;
-        }
-    }
+  private static BufferedImage createRobotScreenshot() {
+      try {
+          LOG.debug("Taking robot screenshot");
+          Robot robot = new Robot();
+          Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+          Rectangle screenRect = new Rectangle(screenSize);
+          BufferedImage shot = robot.createScreenCapture(screenRect);
+          return shot;
+      } catch (Exception e) {
+          LOG.error("Error creating robot screenshot", e);
+          return null;
+      }
+  }
 
-    private static BufferedImage createDbusScreenshot() {
-        try {
-            LOG.debug("Taking DBus Screenshot...");
-            DBusConnection bus = DBusConnection.getConnection(DBusConnection.DBusBusType.SESSION);
-            LOG.debug("Unique name: %s%n", bus.getUniqueName());
+  private static BufferedImage createDbusScreenshot() {
+      try {
+          LOG.debug("Taking DBus Screenshot...");
+          DBusConnection bus = DBusConnection.getConnection(DBusConnection.DBusBusType.SESSION);
+          LOG.debug("Unique name: %s%n", bus.getUniqueName());
 
-            String token = UUID.randomUUID().toString().replaceAll("-", "");
-            String sender = bus.getUniqueName().substring(1).replace('.', '_');
-            String path = String.format("/org/freedesktop/portal/desktop/request/%s/%s", sender, token);
+          String token = UUID.randomUUID().toString().replaceAll("-", "");
+          String sender = bus.getUniqueName().substring(1).replace('.', '_');
+          String path = String.format("/org/freedesktop/portal/desktop/request/%s/%s", sender, token);
 
-            TransferQueue<Optional<BufferedImage>> queue = new LinkedTransferQueue<>();
+          TransferQueue<Optional<BufferedImage>> queue = new LinkedTransferQueue<>();
 
-            DBusMatchRule matchRule = new DBusMatchRule("signal", "org.freedesktop.portal.Request", "Response");
-            bus.addGenericSigHandler(matchRule, new DBusSigHandler<DBusSignal>() {
-                @Override
-                public void handle(DBusSignal t) {
-                    LOG.debug("DBUS signal received");
-                    if (path.equals(t.getPath())) {
-                        try {
-                            Object[] params = t.getParameters();
-                            LOG.debug("params: size=%d%n", params.length);
-                            UInt32 response = (UInt32) params[0];
-                            DBusMap<String, Variant> results = (DBusMap) params[1];
+          DBusMatchRule matchRule = new DBusMatchRule("signal", "org.freedesktop.portal.Request", "Response");
+          bus.addGenericSigHandler(matchRule, new DBusSigHandler<DBusSignal>() {
+              @Override
+              public void handle(DBusSignal t) {
+                  LOG.debug("DBUS signal received");
+                  if (path.equals(t.getPath())) {
+                      try {
+                          Object[] params = t.getParameters();
+                          LOG.debug("params: size=%d%n", params.length);
+                          UInt32 response = (UInt32) params[0];
+                          DBusMap<String, Variant> results = (DBusMap) params[1];
 
-                            if (response.intValue() == 0) {
-                                LOG.debug("Screenshot successful");
-                                Variant vuri = results.get("uri");
-                                String uri = (String) vuri.getValue();
-                                LOG.debug("uri: %s%n", uri);
+                          if (response.intValue() == 0) {
+                              LOG.debug("Screenshot successful");
+                              Variant vuri = results.get("uri");
+                              String uri = (String) vuri.getValue();
+                              LOG.debug("uri: %s%n", uri);
 
-                                BufferedImage shot = ImageIO.read(URI.create(uri).toURL());
-                                queue.add(Optional.of(shot));
+                              BufferedImage shot = ImageIO.read(URI.create(uri).toURL());
+                              queue.add(Optional.of(shot));
 
-                                if (Files.deleteIfExists(Path.of(URI.create(uri)))) {
-                                    LOG.debug("deleted temporary file");
-                                }
-                            } else {
-                                LOG.error("Failed: response=%s%n", response);
-                                queue.add(Optional.empty());
-                            }
+                              if (Files.deleteIfExists(Path.of(URI.create(uri)))) {
+                                  LOG.debug("deleted temporary file");
+                              }
+                          } else {
+                              LOG.error("Failed: response=%s%n", response);
+                              queue.add(Optional.empty());
+                          }
 
-                            bus.removeGenericSigHandler(matchRule, this);
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-            });
+                          bus.removeGenericSigHandler(matchRule, this);
+                      } catch (Exception ex) {
+                          ex.printStackTrace();
+                      }
+                  }
+              }
+          });
 
-            ScreenshotInterface iface = bus.getRemoteObject("org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop", ScreenshotInterface.class);
-            Map<String, Variant> options = new HashMap<>();
-            options.put("interactive", new Variant(Boolean.FALSE));
-            options.put("handle_token", new Variant(token));
-            DBusPath result = iface.Screenshot("", options);
-            LOG.debug("result: %s%n", result);
-            LOG.debug("expected path: %s%n", path);
+          ScreenshotInterface iface = bus.getRemoteObject("org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop", ScreenshotInterface.class);
+          Map<String, Variant> options = new HashMap<>();
+          options.put("interactive", new Variant(Boolean.FALSE));
+          options.put("handle_token", new Variant(token));
+          DBusPath result = iface.Screenshot("", options);
+          LOG.debug("result: %s%n", result);
+          LOG.debug("expected path: %s%n", path);
 
-            Optional<BufferedImage> shotResult = queue.take();
-            if (shotResult.isPresent()) {
-                return shotResult.get();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+          Optional<BufferedImage> shotResult = queue.take();
+          if (shotResult.isPresent()) {
+              return shotResult.get();
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return null;
+  }
 }
